f45900079f9c22ce6d08f0a85d5182f85c001f18
mkbmiyu-gif <mkbmiyu@gmail.com>
2026-06-19 22:14:01 +0900

    Update new_app.js

--- new_app.js
+++ new_app.js
@@ -271,6 +271,24 @@ showDetail(meigiIndex);
 削除
 
 </div>
+<button class="edit-history-btn"
+onclick="editHistory(${index},${hIndex})">
+
+<svg width="16" height="16"
+viewBox="0 0 24 24"
+fill="none"
+stroke="currentColor"
+stroke-width="2"
+stroke-linecap="round"
+stroke-linejoin="round">
+
+<path d="M12 20h9"/>
+<path d="M16.5 3.5a2.1 2.1 0 013 3L7 19l-4 1 1-4 12.5-12.5z"/>
+
+</svg>
+
+</button>
+
 <button
 onclick="editHistory(${index},${hIndex})">
 
